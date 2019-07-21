1ba9baf36aae35e0a204e7ce998b2e60723fd036
Nat <nat@cmmc.io>
2019-07-21 12:09:40 +0700

    just auto format.

--- compiler.js
+++ compiler.js
@@ -10,7 +10,8 @@ let boardDirectory = `${engine.util.boardDir}/${config.name}`;
 let platformDirectory = `${engine.util.platformDir}/${config.platform}`;
 let boardIncludeDir = `${boardDirectory}/include`;
 let platformIncludeDir = `${platformDirectory}/include`;
-let context = JSON.parse(fs.readFileSync(boardDirectory + "/context.json", "utf8"));
+let context = JSON.parse(fs.readFileSync(boardDirectory + "/context.json",
+  "utf8"));
 let platformCompiler = engine.util.requireFunc(`${platformDirectory}/compiler`);
 
 function compile(rawCode, boardName, config, cb) {
@@ -26,19 +27,21 @@ function compile(rawCode, boardName, config, cb) {
     //---- inc folder ----//
     let app_dir = `${boardDirectory}/build/${boardName}`;
     let inc_src = engine.util.walk(boardIncludeDir)
-                             .filter(file => path.extname(file) === ".cpp" || path.extname(file) === ".c");
+      .filter(
+        file => path.extname(file) === ".cpp" || path.extname(file) === ".c");
     inc_src = inc_src.concat(engine.util.walk(platformIncludeDir)
-                             .filter(file => path.extname(file) === ".cpp" || path.extname(file) === ".c"));
+      .filter(
+        file => path.extname(file) === ".cpp" || path.extname(file) === ".c"));
     let inc_switch = [];
     //--- step 1 load template and create full code ---//
-    let sourceCode = null
+    let sourceCode = null;
     let codeContext = null;
     if (config.isSourceCode) {
       sourceCode = rawCode;
       //searching all include to find matched used plugin file
       codeContext = {
         plugins_sources: [],
-        plugins_includes_switch: [],
+        plugins_includes_switch: []
       };
       let pluginInfo = G.plugin.pluginInfo;
       let incsRex = /#include\s*(?:\<|\")(.*?\.h)(?:\>|\")/gm;
@@ -47,12 +50,12 @@ function compile(rawCode, boardName, config, cb) {
         let incFile = m[1].trim();
         //lookup plugin
         let includedPlugin = pluginInfo.categories.filter(
-            obj => obj.sourceFile.includes(incFile));
+          obj => obj.sourceFile.includes(incFile));
         if (includedPlugin.length > 0) {
           codeContext.plugins_includes_switch.push(
-              includedPlugin[0].directory + "/src");
+            includedPlugin[0].directory + "/src");
           let targetCppFile = includedPlugin[0].directory + "/src/" +
-              incFile.replace(".h", ".cpp");
+            incFile.replace(".h", ".cpp");
           codeContext.plugins_sources.push(targetCppFile);
         }
       }
@@ -80,31 +83,31 @@ function compile(rawCode, boardName, config, cb) {
     }
     if (context.ldflags) {
       ldflags = context.ldflags.map(
-          f => f.replace(/\{board\}/g, boardDirectory));
+        f => f.replace(/\{board\}/g, boardDirectory));
     }
     if (context.libflags) {
       libflags = context.libflags.map(
-          f => f.replace(/\{board\}/g, boardDirectory));
+        f => f.replace(/\{board\}/g, boardDirectory));
     }
     //--- step 4 compile
     let contextBoard = {
       board_name: boardName,
       app_dir: app_dir,
       process_dir: boardDirectory,
-      board_context : context,
-      cb,
+      board_context: context,
+      cb
     };
 
     inc_src.push(`${app_dir}/user_app.cpp`);
     platformCompiler.setConfig(contextBoard);
 
     platformCompiler.compileFiles(inc_src, [], cflags, inc_switch)
-    .then(() => {
-      //return platformCompiler.archiveProgram(inc_src);
-      //return engine.util.promiseTimeout(1000);
-    //}).then(() => {
-      return platformCompiler.linkObject(ldflags, libflags);
-    }).then(() => {
+      .then(() => {
+        //return platformCompiler.archiveProgram(inc_src);
+        //return engine.util.promiseTimeout(1000);
+        //}).then(() => {
+        return platformCompiler.linkObject(ldflags, libflags);
+      }).then(() => {
       return platformCompiler.createBin();
     }).then(() => {
       resolve();
@@ -118,7 +121,7 @@ function compile(rawCode, boardName, config, cb) {
 let exp = {};
 Object.assign(exp, platformCompiler);
 Object.assign(exp, {
-  compile,
+  compile
 });
 //console.log(exp);
 module.exports = exp;
--- config.js
+++ config.js
@@ -1,12 +1,12 @@
-module.exports = {    
-    name : 'arduino-uno',
-    platform : 'arduino-avr',
-    title : 'Arduino Uno R3',
-    description : 'Arduino Uno - Beginer board for learning Arduino',
-    author : 'KhonKaenMakerClub',
-    website : 'http://www.arduino.cc',
-    email : 'makerasia@gmail.com',
-    git : 'https://github.com/KhonKaenMakerClub/kbide-arduino-uno/',    
-    image : '/static/display.jpg',
-    version : '1.0.0',
+module.exports = {
+  name: "arduino-uno",
+  platform: "arduino-avr",
+  title: "Arduino Uno R3",
+  description: "Arduino Uno - Beginer board for learning Arduino",
+  author: "KhonKaenMakerClub",
+  website: "http://www.arduino.cc",
+  email: "makerasia@gmail.com",
+  git: "https://github.com/KhonKaenMakerClub/kbide-arduino-uno/",
+  image: "/static/display.jpg",
+  version: "1.0.0"
 };
